d8ce23cbb396fc7661f22929ab423b5f58d73d8e
Matthew Victor <vicmcnabby@gmail.com>
2017-06-21 16:36:18 -0600

    fixed queries

--- db/queries.js
+++ db/queries.js
@@ -19,10 +19,10 @@ module.exports = {
   delete(id) {
     return knex('stadiums').where('id', id).del()
   },
-  getStadiumsByState(state_id){
+  getStadiumsByState(state_id) {
     return knex('stadiums')
       .join('states', 'states.id', 'stadiums.state_id')
-      .select('states.name as state_name','stadiums.name as stadium_name', 'stadiums.id as stadium_id', 'team', 'sport', 'photo')
+      .select('states.name as state_name', 'stadiums.name as stadium_name', 'stadiums.id as stadium_id', 'team', 'sport', 'capacity', 'photo')
       .where('states.id', state_id)
   }
 };
